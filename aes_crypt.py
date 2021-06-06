c8f6f67d756f3afd1c7bbcc1e355a5b3be265d2d
tarequl-islam <itareq38@gmail.com>
2021-06-06 21:59:06 +0600

    initial code

--- aes_crypt.py
+++ aes_crypt.py
@@ -7,13 +7,36 @@ def initial_rounds():
     return
 
 
+def sub_byte():
+    return
+
+
+def shift_rows():
+    return
+
+
+def mix_columns():
+    return
+
+
+def add_round_key():
+    return
+
+
 def aes_encrypt():
     key_expansion()
     initial_rounds()
     number_of_rounds = 1
-    for i in range(number_of_rounds):
-        i = i+1
-
+    
+    for round in range(number_of_rounds): #apply round operations
+        sub_byte()
+        shift_rows()
+        mix_columns()
+        add_round_key()
+    #final round
+    sub_byte()
+    shift_rows()
+    mix_columns()
     return
 
 
