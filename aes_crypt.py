8f4c7b65e21cb66dc7cb6c07ed3a017e3c6d5654
tarequl-islam <itareq38@gmail.com>
2021-06-06 21:08:25 +0600

    Create aes_crypt.py

---
+++ aes_crypt.py
@@ -0,0 +1,21 @@
+
+def key_expansion():
+    return
+
+
+def initial_rounds():
+    return
+
+
+def aes_encrypt():
+    key_expansion()
+    initial_rounds()
+    number_of_rounds = 1
+    for i in range(number_of_rounds):
+        i = i+1
+
+    return
+
+
+message = "This text will be encrypted using aes cryptography!"
+key = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]
