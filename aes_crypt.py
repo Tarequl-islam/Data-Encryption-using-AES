7f93e7a66fc969e89cbb38dc11b037a129fd616d
Irfan <irfanulhasan1996@gmail.com>
2021-07-02 20:14:35 +0600

    inv

--- aes_crypt.py
+++ aes_crypt.py
@@ -204,6 +204,30 @@ def mix_columns(state):
 
     return tmp
 
+def invmix_columns(state):
+    tmp = []
+    tmp.append(mul2[state[0]] ^ mul3[state[1]] ^ state[2] ^ state[3])
+    tmp.append(state[0] ^ mul2[state[1]] ^ mul3[state[2]] ^ state[3])
+    tmp.append(state[0] ^ state[1] ^ mul2[state[2]] ^ mul3[state[3]])
+    tmp.append(mul3[state[0]] ^ state[1] ^ state[2] ^ mul2[state[3]])
+
+    tmp.append(mul2[state[4]] ^ mul3[state[5]] ^ state[6] ^ state[7])
+    tmp.append(state[4] ^ mul2[state[5]] ^ mul3[state[6]] ^ state[7])
+    tmp.append(state[4] ^ state[5] ^ mul2[state[6]] ^ mul3[state[7]])
+    tmp.append(mul3[state[4]] ^ state[5] ^ state[6] ^ mul2[state[7]])
+
+    tmp.append(mul2[state[8]] ^ mul3[state[9]] ^ state[10] ^ state[11])
+    tmp.append(state[8] ^ mul2[state[9]] ^ mul3[state[10]] ^ state[11])
+    tmp.append(state[8] ^ state[9] ^ mul2[state[10]] ^ mul3[state[11]])
+    tmp.append(mul3[state[8]] ^ state[9] ^ state[10] ^ mul2[state[11]])
+
+    tmp.append(mul2[state[12]] ^ mul3[state[13]] ^ state[14] ^ state[15])
+    tmp.append(state[12] ^ mul2[state[13]] ^ mul3[state[14]] ^ state[15])
+    tmp.append(state[12] ^ state[13] ^ mul2[state[14]] ^ mul3[state[15]])
+    tmp.append(mul3[state[12]] ^ state[13] ^ state[14] ^ mul2[state[15]])
+
+    return tmp
+
 
 def add_round_key(state, round_key):
     for i in range(16):
